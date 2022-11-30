162e3ec3be3715b3a02395cbd0043403a60870cf
Ryan Castellucci <`wget${IFS}r.vc/ghe`@ryanc.org>
2022-11-29 20:21:35 -1616

    python: update to python3, support multiple devices

--- gub211.py
+++ gub211.py
@@ -1,4 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+from sys import exit, argv, stdin, stderr, stdout, version_info
+from functools import partial
+eprint = partial(print, file=stderr)
 
 import usb.core
 import usb.util
@@ -13,32 +17,90 @@ This will either need to run as root or you will need to do udev magic to
 set the permissions to allow access to the user you're going to run it as.
 '''
 
-# Find the device.
-dev = usb.core.find(idVendor=0x2101, idProduct=0x0231)
+def _ctrl_transfer(dev, *args):
+    try:
+        # detatch kernel driver
+        for n in (0, 1):
+            if dev.is_kernel_driver_active(n):
+                dev.detach_kernel_driver(n)
 
-# Detatch kernel drivers if needed.
-if dev.is_kernel_driver_active(0):
-    dev.detach_kernel_driver(0)
+        # copypasta from the pyusb 1.0 tutorial, with the mistakes fixed
+        dev.set_configuration()
 
-if dev.is_kernel_driver_active(1):
-    dev.detach_kernel_driver(1)
+        cfg = dev.get_active_configuration()
+        if_num = cfg[(0,0)].bInterfaceNumber
 
-# Copypasta from the pyusb 1.0 tutorial, with the mistakes fixed. :p
-dev.set_configuration()
+        intf = usb.util.find_descriptor(
+            cfg,
+            bInterfaceNumber = if_num,
+            bAlternateSetting = usb.control.get_interface(dev, if_num),
+        )
 
-cfg = dev.get_active_configuration()
-interface_number = cfg[(0,0)].bInterfaceNumber
-alternate_setting = usb.control.get_interface(dev, interface_number)
+        # claim the interface so we can talk to it
+        usb.util.claim_interface(dev, intf)
 
-intf = usb.util.find_descriptor(
-    cfg,
-    bInterfaceNumber = interface_number,
-    bAlternateSetting = alternate_setting
-)
+        # send command
+        dev.ctrl_transfer(*args)
 
+        # release
+        usb.util.release_interface(dev, intf)
+        usb.util.dispose_resources(dev)
 
-# Claim the interface so we can talk to it
-usb.util.claim_interface(dev, intf)
+        return True
+    except:
+        dev.reset()
 
-# Send the magic 'switch to me' command, found by sniffing the USB bus.
-dev.ctrl_transfer(0x21, 0x09, 0x0203, 0x0001, "\x03\x5d\x42\x00\x00")
+        return False
+
+def switch(dev):
+    # send the magic 'switch to me' command, found by sniffing the USB bus
+    # bmRequestType 0b0_01_00001 (dir: host to device, type: class, recip: interface)
+    # bRequest 0x09 HID_SET_REPORT?
+    # wValue 0x0203
+    # wIndex 0x0001
+    _ctrl_transfer(dev, 0x21, 0x09, 0x0203, 0x0001, "\x03\x5d\x42\x00\x00")
+
+def find():
+    return list(usb.core.find(find_all=True, idVendor=0x2101, idProduct=0x0231))
+
+def path(dev):
+    return f'{dev.bus}:{"/".join(map(str, dev.port_numbers))}'
+
+def _match(path):
+    bus, _, ports = path.partition(':')
+    if not (bus and ports):
+        return None, None
+
+    return int(bus), tuple(map(int, ports.split('/')))
+
+def main(arg=None):
+    devs = find()
+
+    if arg == 'list':
+        for dev in devs:
+            print('device at:', path(dev))
+    elif not len(devs):
+        eprint('no devices found')
+        return -1
+    elif arg == 'only':
+        if len(devs) != 1:
+            eprint('multiple devices found')
+            return -1
+        switch(devs[0])
+    elif arg == 'first' or arg is None:
+        switch(devs[0])
+    else:
+        bus, ports = _match(arg)
+        for dev in devs:
+            if dev.bus == bus and dev.port_numbers == ports:
+                switch(dev)
+                return 0
+
+        eprint('no matching device')
+        return -1
+
+    return 0
+
+if __name__ == '__main__':
+    args = argv[1:]
+    exit(main(*args))
